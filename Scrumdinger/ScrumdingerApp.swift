f781beee16cf8cb9a3f2baecbf7bf0d0cfb166ee
Anish Kumar Dubey <kumaranishdubey@gmail.com>
2022-11-29 08:25:34 +0530

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Anish kumar dubey on 29/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
